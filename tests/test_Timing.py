b02b773cf9944f309018fa72a39480fca31befa1
Felix Stutz <fstutz@mpi-inf.mpg.de>
2019-10-25 00:54:05 +0200

    final benchmark settings

--- tests/test_Timing.py
+++ tests/test_Timing.py
@@ -1,70 +1,70 @@
 from Timing import *
 from collections import OrderedDict
 
-prefix = "../benchmarks/"
+prefix = "benchmarks/"
 
 def test_get_benchmark_times():
-    # Change here to adjust number of samples
+    # Change here to adjust number of samples; benchmarks taken with num_samples = 10
     num_samples = 1
     pairs = OrderedDict({
-            # "ARPC":
-            #     ("Andrew-Secure-RPC/Andrew-RPC-1-widen.txt", True,
-            #      "Andrew-Secure-RPC/Andrew-RPC-2-invariant.txt",
-            #      "secret", "F"),
-            # "KSL":
-            #     ("Kehne-Schoenwaelder-Landendoerfer/KSL-1-without-reauth-widen.txt", True,
-            #      "Kehne-Schoenwaelder-Landendoerfer/KSL-2-without-reauth-invariant.txt",
-            #      "secret", "F"),
-            # "KSLR":
-            #     ("Kehne-Schoenwaelder-Landendoerfer/KSL-3-continuous-reauth-widen.txt", True,
-            #      "Kehne-Schoenwaelder-Landendoerfer/KSL-4-continuous-reauth-invariant.txt",
-            #      "secret", "F"),
-            # "NHS":
-            #     ("Needham-Schroeder/Needham-Schroeder-1-widen.txt", True,
-            #      "Needham-Schroeder/Needham-Schroeder-2-invariant.txt",
-            #      "not modelled", "F"),
-            # "NHSs":
-            #     ("Needham-Schroeder/Needham-Schroeder-Secret-1-widen.txt", True,
-            #      "Needham-Schroeder/Needham-Schroeder-Secret-2-invariant.txt",
-            #      "secret", "F"),
-            # "NHSr":
-            #     ("Needham-Schroeder/Needham-Schroeder-Replay-1-widen.txt", False,
-            #      "Needham-Schroeder/Needham-Schroeder-Replay-2-invariant.txt",
-            #      "leaky", "I"),
-            # "OR":
-            #     ("Otway-Rees/Otway-Rees-CSF-1-widen.txt", True,
-            #      "Otway-Rees/Otway-Rees-CSF-2-invariant.txt",
-            #      "not modelled", "F"),
-            # "ORl":
-            #     ("Otway-Rees/Otway-Rees-Leaky-1-widen.txt", True,
-            #      "Otway-Rees/Otway-Rees-Leaky-2-invariant.txt",
-            #      "not secret", "F"),
+             "ARPC":
+                 ("Andrew-Secure-RPC/Andrew-RPC-1-widen.txt", True,
+                  "Andrew-Secure-RPC/Andrew-RPC-2-invariant.txt",
+                  "secret", "F"),
+             "KSL":
+                 ("Kehne-Schoenwaelder-Landendoerfer/KSL-1-without-reauth-widen.txt", True,
+                  "Kehne-Schoenwaelder-Landendoerfer/KSL-2-without-reauth-invariant.txt",
+                  "secret", "F"),
+             "KSLR":
+                 ("Kehne-Schoenwaelder-Landendoerfer/KSL-3-continuous-reauth-widen.txt", True,
+                  "Kehne-Schoenwaelder-Landendoerfer/KSL-4-continuous-reauth-invariant.txt",
+                  "secret", "F"),
+             "NHS":
+                 ("Needham-Schroeder/Needham-Schroeder-1-widen.txt", True,
+                  "Needham-Schroeder/Needham-Schroeder-2-invariant.txt",
+                  "not modelled", "F"),
+             "NHSs":
+                 ("Needham-Schroeder/Needham-Schroeder-Secret-1-widen.txt", True,
+                  "Needham-Schroeder/Needham-Schroeder-Secret-2-invariant.txt",
+                  "secret", "F"),
+             "NHSr":
+                 ("Needham-Schroeder/Needham-Schroeder-Replay-1-widen.txt", False,
+                  "Needham-Schroeder/Needham-Schroeder-Replay-2-invariant.txt",
+                  "leaky", "I"),
+             "OR":
+                 ("Otway-Rees/Otway-Rees-CSF-1-widen.txt", True,
+                  "Otway-Rees/Otway-Rees-CSF-2-invariant.txt",
+                  "not modelled", "F"),
+             "ORl":
+                 ("Otway-Rees/Otway-Rees-Leaky-1-widen.txt", True,
+                  "Otway-Rees/Otway-Rees-Leaky-2-invariant.txt",
+                  "not secret", "F"),
             "ORa":
                 ("Otway-Rees/Otway-Rees-Auth-1-widen.txt", True,
                  "Otway-Rees/Otway-Rees-Auth-2-invariant.txt",
                  "not modelled", "I"),
-            # "ORs":
-            #     ("Otway-Rees/Otway-Rees-Secret-1-widen.txt", True,
-            #      "Otway-Rees/Otway-Rees-Secret-2-invariant.txt",
-            #      "secret", "F"),
-            # "Ex.9":
-            #     ("Running-Example-Paper/Running-Example-1-widen.txt", True,
-            #      "Running-Example-Paper/Running-Example-3-invariant.txt",
-            #      "secret", "F"),
-            # "YAH":
-            #     ("Yahalom/Yahalom-CSF-1-widen.txt", True,
-            #      "Yahalom/Yahalom-CSF-2-invariant.txt", "not modelled", "F"),
-            # "YAHs1":
-            #     ("Yahalom/Yahalom-Secret-ny-not-leaked-1-widen.txt", True,
-            #      "Yahalom/Yahalom-Secret-ny-not-leaked-2-invariant.txt", "secret", "F"),
-            # "YAHs2":
-            #     ("Yahalom/Yahalom-Secret-size-key-1-widen.txt", True,
-            #      "Yahalom/Yahalom-Secret-size-key-2-invariant.txt", "secret", "F"),
-            # "YAHlk":
-            #     ((("Yahalom/Yahalom-Leaked-Key-Variant-1-Non-Leaky-widen.txt", True),
-            #      ("Yahalom/Yahalom-Leaked-Key-Variant-1-Leaky-widen.txt", True)),
-            #      "Yahalom/Yahalom-Leaked-Key-Variant-2-A+B-merged-invariant.txt",
-            #      "secret", "I")
+             "ORs":
+                 ("Otway-Rees/Otway-Rees-Secret-1-widen.txt", True,
+                  "Otway-Rees/Otway-Rees-Secret-2-invariant.txt",
+                  "secret", "F"),
+             "Ex.9":
+                 ("Running-Example-Paper/Running-Example-1-widen.txt", True,
+                  "Running-Example-Paper/Running-Example-3-invariant.txt",
+                  "secret", "F"),
+             "YAH":
+                 ("Yahalom/Yahalom-CSF-1-widen.txt", True,
+                  "Yahalom/Yahalom-CSF-2-invariant.txt", "not modelled", "F"),
+             "YAHs1":
+                 ("Yahalom/Yahalom-Secret-ny-not-leaked-1-widen.txt", True,
+                  "Yahalom/Yahalom-Secret-ny-not-leaked-2-invariant.txt", "secret", "F"),
+             "YAHs2":
+                 ("Yahalom/Yahalom-Secret-size-key-1-widen.txt", True,
+                  "Yahalom/Yahalom-Secret-size-key-2-invariant.txt", "secret", "F"),
+             "YAHlk":
+                 ((("Yahalom/Yahalom-Leaked-Key-Variant-1-Non-Leaky-widen.txt", True),
+                  ("Yahalom/Yahalom-Leaked-Key-Variant-1-Leaky-widen.txt", True)),
+                  "Yahalom/Yahalom-Leaked-Key-Variant-2-A+B-merged-invariant.txt",
+                  "secret", "I")
     })
     results = "\nName \t\tInfer \t\tC \t\tS \n"
     for (name, (fws, fwe, fi, model, how)) in pairs.iteritems():
